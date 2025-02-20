374fc9f9832f04cf23e45e8b8c4230bba8ca4555
ambroggi <abroggi@umassd.edu>
2025-02-20 11:29:54 -0500

    Add boundary tests
    Test all boundaries of the function "classifyTriangle
    "

--- classifyTriangle.test.js
+++ classifyTriangle.test.js
@@ -18,3 +18,85 @@ describe('classifyTriangle: Required input conditions -  See readme for more det
     });
   });
   
+  describe('classifyTriangle: Boundary tests', () => {
+    test('should classify an Equilateral triangle', () => {
+      expect(classifyTriangle(10, 10, 10)).toBe('Equilateral');
+    });
+  
+    test('First bound low inside', () => {
+      expect(classifyTriangle(1.00001, 10, 10)).toBe('Isosceles');
+    });
+  
+    test('First bound low on the line', () => {
+      expect(classifyTriangle(1, 10, 10)).toBe('Isosceles');
+    });
+
+    test('First bound low outside', () => {
+      expect(classifyTriangle(0.99999, 10, 10)).toBe('Error: Input conditions C1, C2, or C3 failed.');
+    });
+
+
+    test('Second bound low inside', () => {
+      expect(classifyTriangle(10, 1.00001, 10)).toBe('Isosceles');
+    });
+  
+    test('Second bound low on the line', () => {
+      expect(classifyTriangle(10, 1, 10)).toBe('Isosceles');
+    });
+
+    test('Second bound low outside', () => {
+      expect(classifyTriangle(10, 0.99999, 10)).toBe('Error: Input conditions C1, C2, or C3 failed.');
+    });
+
+
+    test('Third bound low inside', () => {
+      expect(classifyTriangle(10, 10, 1.00001)).toBe('Isosceles');
+    });
+  
+    test('Third bound low on the line', () => {
+      expect(classifyTriangle(10, 10, 1)).toBe('Isosceles');
+    });
+
+    test('Third bound low outside', () => {
+      expect(classifyTriangle(10, 10, 0.99999)).toBe('Error: Input conditions C1, C2, or C3 failed.');
+    });
+
+
+    test('First bound high inside', () => {
+      expect(classifyTriangle(199.99999, 10, 10)).toBe('Not a Triangle');
+    });
+  
+    test('First bound high on the line', () => {
+      expect(classifyTriangle(200, 10, 10)).toBe('Not a Triangle');
+    });
+
+    test('First bound high outside', () => {
+      expect(classifyTriangle(200.00001, 10, 10)).toBe('Error: Input conditions C1, C2, or C3 failed.');
+    });
+
+
+    test('Second bound high inside', () => {
+      expect(classifyTriangle(10, 199.99999, 10)).toBe('Not a Triangle');
+    });
+  
+    test('Second bound high on the line', () => {
+      expect(classifyTriangle(10, 200, 10)).toBe('Not a Triangle');
+    });
+
+    test('Second bound high outside', () => {
+      expect(classifyTriangle(10, 200.00001, 10)).toBe('Error: Input conditions C1, C2, or C3 failed.');
+    });
+
+
+    test('Third bound high inside', () => {
+      expect(classifyTriangle(10, 10, 199.99999)).toBe('Not a Triangle');
+    });
+  
+    test('Third bound high on the line', () => {
+      expect(classifyTriangle(10, 10, 200)).toBe('Not a Triangle');
+    });
+
+    test('Third bound high outside', () => {
+      expect(classifyTriangle(10, 10, 200.00001)).toBe('Error: Input conditions C1, C2, or C3 failed.');
+    });
+  });
